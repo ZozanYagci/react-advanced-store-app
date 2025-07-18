d568fc0f9b0d1c1725bcf8ba02caa4002c87333a
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-07-18 14:00:31 +0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import HomePage from "./pages/Home";
+import CartPage from "./pages/Cart";
+import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
+import ProductsPage from "./pages/Products";
+import MainLayout from "./layouts/MainLayout";
+import ProductDetailsPage from "./pages/ProductDetails";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "home", element: <HomePage /> },
+      {
+        path: "products",
+        children: [
+          { index: true, element: <ProductsPage /> },
+          { path: ":id", element: <ProductDetailsPage /> },
+        ],
+      },
+
+      { path: "cart", element: <CartPage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "register", element: <RegisterPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return <h1>Store App</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
