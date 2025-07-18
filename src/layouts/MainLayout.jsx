7d1822fc1f7e0eba6ca914b4bf2e5e518ded9817
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-07-18 15:39:16 +0300

    install Material UI

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import ButtonUsage from "../components/ButtonUsage";
 
 export default function MainLayout() {
   return (
     <div className="container">
+      <ButtonUsage></ButtonUsage>
       <h1>Main Layout</h1>
       <Outlet></Outlet>
     </div>
