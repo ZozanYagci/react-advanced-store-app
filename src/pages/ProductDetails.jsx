2eb87965ae62b7549706906ce74dbf5f3bb803a4
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-07-21 00:18:57 +0300

    product details page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import ProductItem from "../components/ProductItem";
+
 export default function ProductDetailsPage() {
-  return <h1>ProductDetailsPage</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    async function fetchProductDetail() {
+      try {
+        const response = await fetch("http://localhost:5000/products/" + id);
+        const data = await response.json();
+        setProduct(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProductDetail();
+  }, [id]);
+
+  if (loading) return <h1>Loading....</h1>;
+
+  return <ProductItem product={product}></ProductItem>;
 }
