2aad757691c6252779e51587ac62e616e4fbe2de
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-07-20 23:09:55 +0300

    product card

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,15 +1,12 @@
 import { Grid } from "@mui/material";
+import ProductCard from "./ProductCard";
 
 export default function ProductList({ products }) {
   return (
     <Grid container spacing={2}>
       {products.map((p) => (
-        <Grid
-          sx={{ backgroundColor: "primary.light" }}
-          key={p.id}
-          size={{ xs: 6, md: 4, lg: 3 }}
-        >
-          {p.title}
+        <Grid key={p.id} size={{ xs: 6, md: 4, lg: 3 }}>
+          <ProductCard product={p}></ProductCard>
         </Grid>
       ))}
     </Grid>
