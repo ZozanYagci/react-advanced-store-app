04922665ea496ada6fca6aebe87a56644c283bcf
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-07-22 23:29:56 +0300

    server errors

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ import RegisterPage from "./pages/Register";
 import ProductsPage from "./pages/Products";
 import MainLayout from "./layouts/Main";
 import ProductDetailsPage from "./pages/ProductDetails";
-import ErrorPage from "./pages/ErrorPage";
+import ErrorPage from "./pages/errors/Error";
+import ServerErrorPage from "./pages/errors/ServerError";
 
-const router = createBrowserRouter([
+export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
@@ -26,7 +27,13 @@ const router = createBrowserRouter([
       { path: "cart", element: <CartPage /> },
       { path: "login", element: <LoginPage /> },
       { path: "register", element: <RegisterPage /> },
-      { path: "errors", element: <ErrorPage /> },
+      {
+        path: "errors",
+        children: [
+          { index: true, element: <ErrorPage /> },
+          { path: "server-error", element: <ServerErrorPage/> },
+        ],
+      },
     ],
   },
 ]);
