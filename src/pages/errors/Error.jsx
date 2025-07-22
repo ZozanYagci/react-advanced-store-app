93033f503ebbb9b8ee06d7bf1d99df48e37cce4d
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-07-23 00:51:45 +0300

    validation errors

--- src/pages/errors/Error.jsx
+++ src/pages/errors/Error.jsx
@@ -1,9 +1,39 @@
-import { Box, Button } from "@mui/material";
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 import requests from "../../api/apiClient";
+import { useState } from "react";
 
 export default function ErrorPage() {
+  const [validationError, setValidationError] = useState({});
+
+  function getValidationErrors() {
+    requests.errors.get403Error().catch((data) => {
+      setValidationError(data);
+    });
+  }
+
   return (
     <Box>
+      {validationError && validationError.errors && (
+        <Alert severity="error" sx={{ mb: 2 }}>
+          <AlertTitle>{validationError.message}</AlertTitle>
+          <List>
+            {validationError.errors.map((error, index) => (
+              <ListItem key={index}>
+                <ListItemText>{error}</ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Alert>
+      )}
+
       <Button
         sx={{ mr: 2 }}
         variant="outlined"
@@ -26,7 +56,7 @@ export default function ErrorPage() {
         sx={{ mr: 2 }}
         variant="outlined"
         color="error"
-        onClick={() => requests.errors.get403Error()}
+        onClick={getValidationErrors}
       >
         Validation Error
       </Button>
