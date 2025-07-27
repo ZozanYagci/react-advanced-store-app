ff2f56315105b483dce843c42a4d1005cf7d3a1e
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-07-27 19:38:56 +0300

    add to cart feature implemented

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import ProductDetailsPage from "./pages/ProductDetails";
 import ErrorPage from "./pages/errors/Error";
 import ServerErrorPage from "./pages/errors/ServerError";
 import NotFoundPage from "./pages/errors/NotFound";
+import { useEffect } from "react";
+import requests from "./api/apiClient";
 
 export const router = createBrowserRouter([
   {
@@ -42,6 +44,12 @@ export const router = createBrowserRouter([
 ]);
 
 function App() {
+  useEffect(() => {
+    requests.cart
+      .get()
+      .then((cart) => console.log(cart))
+      .catch((error) => console.log(error));
+  }, []);
   return <RouterProvider router={router} />;
 }
 
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,6 +5,7 @@ import {
   CardActions,
   CardContent,
   CardMedia,
+  CircularProgress,
   IconButton,
   Typography,
 } from "@mui/material";
@@ -12,8 +13,19 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { Link } from "react-router";
 import { currenyTRY } from "../utils/formats";
+import { useState } from "react";
+import requests from "../api/apiClient";
 
 export default function ProductCard({ product }) {
+  const [loading, setLoading] = useState(false);
+  function handleAddItem(productId) {
+    setLoading(true);
+    requests.cart
+      .addItem(productId)
+      .then((cart) => console.log(cart))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }
   return (
     <Card>
       <CardActionArea component={Link} to={"/products/" + product.id}>
@@ -43,7 +55,9 @@ export default function ProductCard({ product }) {
           {/* <FavoriteIcon></FavoriteIcon> */}
           <FavoriteBorderIcon></FavoriteBorderIcon>
         </IconButton>
-        <Button>Sepete Ekle</Button>
+        <Button onClick={() => handleAddItem(product.id)}>
+          {loading ? <CircularProgress size="20px" /> : "Sepete Ekle"}
+        </Button>
       </CardActions>
     </Card>
   );
