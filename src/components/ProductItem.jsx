81c701b7f31609ce6bbb590ae7bd225115144ca6
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-07-29 22:52:55 +0300

    cart update on product detail page

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,6 +1,20 @@
-import { Grid, Paper, Typography } from "@mui/material";
+import {
+  Button,
+  CircularProgress,
+  Grid,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { currenyTRY } from "../utils/formats";
+import ReportIcon from "@mui/icons-material/Report";
 
-export default function ProductItem({ product }) {
+export default function ProductItem({
+  product,
+  handleAddItem,
+  cartItem,
+  isAdding,
+}) {
   return (
     <Grid container spacing={2}>
       <Grid size={{ lg: 4, md: 5, sm: 6, xs: 12 }}>
@@ -17,6 +31,36 @@ export default function ProductItem({ product }) {
             {product.title}
           </Typography>
           <Typography variant="body1">{product.description}</Typography>
+          <Typography variant="h5" color="secondary" sx={{ mt: 3 }}>
+            {currenyTRY.format(product.price)}
+          </Typography>
+          <Stack
+            direction="row"
+            display="flex"
+            alignItems="center"
+            gap={2}
+            sx={{ mt: 3 }}
+          >
+            <Button
+              onClick={() => handleAddItem(product.id)}
+              variant="contained"
+              color="secondary"
+            >
+              Sepete Ekle
+            </Button>
+
+            {cartItem?.product.quantity > 0 && (
+              <Typography
+                variant="body2"
+                sx={{ display: "flex", alignItems: "center" }}
+              >
+                <ReportIcon color="secondary"></ReportIcon> Sepetinize{" "}
+                {cartItem.product.quantity} adet eklendi.
+              </Typography>
+            )}
+
+            {isAdding && <CircularProgress size="20px"></CircularProgress>}
+          </Stack>
         </Paper>
       </Grid>
     </Grid>
