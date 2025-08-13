2621d9c3c04b029479674e49ef24dcb401127b6a
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-08-13 20:45:01 +0300

    add address form, payment form and review section to checkout page

--- src/pages/checkout/AddressForm.jsx
+++ src/pages/checkout/AddressForm.jsx
@@ -1,3 +1,76 @@
+import { Grid, TextField } from "@mui/material";
+import { useFormContext } from "react-hook-form";
+
 export default function AddressForm() {
-  return <h1>AddressForm</h1>;
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  return (
+    <Grid container spacing={3}>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <TextField
+          {...register("firstname", {
+            required: "firstname zorunlu alan",
+          })}
+          label="Enter firstname"
+          size="small"
+          fullWidth
+          autoFocus
+          sx={{ mb: 2 }}
+          error={!!errors.firstname}
+        ></TextField>
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <TextField
+          {...register("lastname", {
+            required: "lastname zorunlu alan",
+          })}
+          label="Enter lastname"
+          size="small"
+          fullWidth
+          sx={{ mb: 2 }}
+          error={!!errors.lastname}
+        ></TextField>
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <TextField
+          {...register("phone", {
+            required: "phone zorunlu alan",
+          })}
+          label="Enter phone"
+          size="small"
+          fullWidth
+          sx={{ mb: 2 }}
+          error={!!errors.phone}
+        ></TextField>
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <TextField
+          {...register("city", {
+            required: "city zorunlu alan",
+          })}
+          label="Enter city"
+          size="small"
+          fullWidth
+          sx={{ mb: 2 }}
+          error={!!errors.city}
+        ></TextField>
+      </Grid>
+      <Grid size={{ xs: 12 }}>
+        <TextField
+          {...register("address", {
+            required: "address zorunlu alan",
+          })}
+          label="Enter address"
+          size="small"
+          fullWidth
+          multiline
+          rows={4}
+          sx={{ mb: 2 }}
+          error={!!errors.address}
+        ></TextField>
+      </Grid>
+    </Grid>
+  );
 }
